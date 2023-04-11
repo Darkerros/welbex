07daf6e612b9860e0208c1135a13914b516f529e
darker <darkerros@yandex.ru>
2023-04-11 18:26:06 +0300

    fix: fix element pos and add light

--- src/modules/app/app.tsx
+++ src/modules/app/app.tsx
@@ -2,12 +2,16 @@ import styles from './app.module.scss'
 import {Header} from "../../components/header";
 import {MainContent} from "../../components/main-content";
 import {Footer} from "../../components/footer";
+import {Light} from "../../components/light/light";
 
 function App() {
   return (
     <div className={styles.app}>
+
       <Header/>
-      <main>
+      <main className={styles.app__main}>
+          <Light extraClass={styles.app__light_red}/>
+          <Light extraClass={styles.app__light_purple}/>
           <MainContent/>
       </main>
       <Footer/>
