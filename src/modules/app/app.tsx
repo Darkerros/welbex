bc21be70f17cf224aea667cdaf27f14842d942d9
darker <darkerros@yandex.ru>
2023-04-11 12:48:32 +0300

    feat: add font import and add text gradients

--- src/modules/app/app.tsx
+++ src/modules/app/app.tsx
@@ -1,7 +1,14 @@
+import styles from './app.module.scss'
+import {Header} from "../../components/header";
+import {MainContent} from "../../components/main-content";
+
 function App() {
   return (
-    <div>
-
+    <div className={styles.app}>
+      <Header/>
+      <main>
+          <MainContent/>
+      </main>
     </div>
   );
 }
