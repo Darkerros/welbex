aff316b17f7201f49ef5c420fa2a4c69562af285
darker <darkerros@yandex.ru>
2023-04-11 15:14:30 +0300

    feat: add adaptive to header

--- src/components/header/components/header-logo/header-logo.tsx
+++ src/components/header/components/header-logo/header-logo.tsx
@@ -1,7 +1,12 @@
 import {Logo} from "../../../logo/logo";
 import styles from './header-logo.module.scss'
+import {useMatchMedia} from "../../../../hooks/use-match-media";
 
 export const HeaderLogo = () => {
+    const isNeedHide = useMatchMedia("(max-width: 750px)")
+
+    if (isNeedHide) return null
+
     return (
         <div className={styles.headerLogo}>
             <Logo/>
--- src/components/header/components/header-socials/header-socials.tsx
+++ src/components/header/components/header-socials/header-socials.tsx
@@ -1,11 +1,16 @@
 import styles from "./header-socials.module.scss"
 import {Socials} from "../../../socials/socials";
+import {useMatchMedia} from "../../../../hooks/use-match-media";
 
 export const HeaderSocials = () => {
+    const isNeedHide = useMatchMedia("(max-width: 925px)")
+
+    if (isNeedHide) return null
+
     return (
         <div className={styles.headerSocials}>
             <a className={styles.headerSocials__tel} href="tel:+7 555 555-55-55">+7 555 555-55-55</a>
-            <Socials/>
+            <Socials />
         </div>
     );
 };
